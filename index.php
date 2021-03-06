f8fd9df37bd464b8b7eec93b97abfddf0d161261
manchesterphan <hieupvtgcs190009@fpt.edu.vn>
2021-03-06 10:12:27 +0700

    Revert "Revert "Update index.php""
    This reverts commit aa17dce3b78bf07f8a48eca3f7a7f94f613e71ce.

--- index.php
+++ index.php
@@ -10,27 +10,16 @@
 </head>
 
 <body>
+	<form method="post" action="Productscript.php">
+	Id: <input type="text" name="id" id="id"><br>
+	Product Name: <input type="text" name="productname" id="productname"><br>
+	Category: <input type="text" name="category" id="category"><br>
+	Price: <input type="text" name="price" id="price"><br>
+	Descriptions: <input type="text" name="descriptions" id="descriptions"><br>
+<input type="submit">
+</form>
 	
-	<ul id="mypetlist" data-role="listview">
-		<li><a>This is an item 1</a></li>
-		<li><a>This is an item 2</a></li>
-		<li><a>This is an item 3</a></li>
-		<li><a>This is an item 4</a></li>
-
-	</ul>
-
-	<a id="btnparrot" href="" class="ui-btn">Add a Parrot</a>
-	<button class="ui-btn ui-icon-delete ui-btn-icon-left">Add a Dog</button>
-
-	<script>
-		//$(document): equal to Javascript document.
-		//$(document).ready(...) called when this document is loaded
-		$("#btnparrot").click(addParrot); //equivalent to Javascript document.getElementById("btnparrot").onclick = ...;
-
-		function addParrot(){
-			$("#mypetlist").append("<li><a>a Dog</a><button>View</button></li>").listview("refresh");
 			
-		}
-	</script>
+		
 </body>
 </html>
