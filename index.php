09b5b8de988796a8993eea6d12fef81fee0d90b4
manchesterphan <hieupvtgcs190009@fpt.edu.vn>
2021-03-06 10:11:11 +0700

    Revert "Delete index.php"
    This reverts commit bd06148fbaaff7b92c173b1205225efd0ba78038.

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>JQuery Mobile Lab 2</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css" />
+	<script src="jquery.mobile-1.4.5/jquery-1.11.1.min.js"></script>
+	<script src="jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+
+<body>
+	<form method="post" action="Productscript.php">
+	Id: <input type="text" name="id" id="id"><br>
+	Product Name: <input type="text" name="productname" id="productname"><br>
+	Category: <input type="text" name="category" id="category"><br>
+	Price: <input type="text" name="price" id="price"><br>
+	Descriptions: <input type="text" name="descriptions" id="descriptions"><br>
+<input type="submit">
+</form>
+	
+			
+		
+</body>
+</html>
